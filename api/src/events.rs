3ef1f9102f92d874e075212a244d258ab9bffd4c
imabdulbasit <baronbasit@gmail.com>
2023-04-28 04:36:09 +0500

    consume deduct_credits and produce credits_deducted event

--- api/src/events.rs
+++ api/src/events.rs
@@ -1,10 +1,16 @@
-use hub_core::{prelude::*, uuid::Uuid};
-use sea_orm::{ActiveModelTrait, Set};
+use std::ops::Sub;
+
+use hub_core::{prelude::*, producer::Producer, uuid::Uuid};
+use sea_orm::{ActiveModelTrait, EntityTrait, Set};
 
 use crate::{
+    credits::{credits_event, Action, Blockchain, Credits, CreditsEvent, CreditsEventKey},
     db::Connection,
-    entities::{credit_deposits, organization_credits, sea_orm_active_enums::DepositReason},
-    proto::{organization_events, Organization, OrganizationEventKey},
+    entities::{
+        credit_deductions, credit_deposits, organization_credits,
+        sea_orm_active_enums::{self, DepositReason},
+    },
+    proto::{credits_mpsc_event, organization_events, Organization, OrganizationEventKey},
     Services,
 };
 /// This function matches each event type and processes it.
@@ -12,7 +18,12 @@ use crate::{
 /// # Errors
 /// This function fails if it is unable to process any event
 #[allow(clippy::too_many_lines)]
-pub async fn process(msg: Services, db: Connection, gift_amount: u64) -> Result<()> {
+pub async fn process(
+    msg: Services,
+    db: Connection,
+    producer: Producer<CreditsEvent>,
+    gift_amount: u64,
+) -> Result<()> {
     // match topics
     match msg {
         Services::Organizations(key, e) => match e.event {
@@ -20,6 +31,12 @@ pub async fn process(msg: Services, db: Connection, gift_amount: u64) -> Result<
                 deposit_gifted_credits(db, key, org, gift_amount).await
             },
             Some(_) | None => Ok(()),
+        },
+        Services::CreditsMpsc(key, e) => match e.event {
+            Some(credits_mpsc_event::Event::DeductCredits(c)) => {
+                deduct_credits(db, producer, key, c).await
+            },
+            None => Ok(()),
         },
     }
 }
@@ -57,4 +74,81 @@ async fn deposit_gifted_credits(
     org_credits.insert(db.get()).await?;
 
     Ok(())
+}
+
+async fn deduct_credits(
+    db: Connection,
+    producer: Producer<CreditsEvent>,
+    key: CreditsEventKey,
+    c: Credits,
+) -> Result<()> {
+    let CreditsEventKey { id, user_id } = key.clone();
+
+    let Credits {
+        credits,
+        action,
+        blockchain,
+    } = c;
+
+    let org_id = Uuid::from_str(&id)?;
+    let user_id = Uuid::from_str(&user_id)?;
+
+    let org_credits_model = organization_credits::Entity::find_by_id(org_id)
+        .one(db.get())
+        .await?
+        .ok_or_else(|| anyhow!("No organization found with id {}", org_id))?;
+    let mut org_credits: organization_credits::ActiveModel = org_credits_model.clone().into();
+
+    org_credits.balance = Set(org_credits_model.balance.sub(credits));
+
+    let action = Action::from_i32(action).ok_or_else(|| anyhow!("Invalid action: {}", action))?;
+    let blockchain = Blockchain::from_i32(blockchain)
+        .ok_or_else(|| anyhow!("Unsupported blockchain: {}", blockchain))?;
+
+    let credit_deductions = credit_deductions::ActiveModel {
+        organization: Set(org_id),
+        initiated_by: Set(user_id),
+        credits: Set(credits),
+        action: Set(action.try_into()?),
+        blockchain: Set(blockchain.try_into()?),
+        ..Default::default()
+    };
+
+    credit_deductions.insert(db.get()).await?;
+    org_credits.update(db.get()).await?;
+
+    let event = CreditsEvent {
+        event: Some(credits_event::Event::CreditsDeducted(c)),
+    };
+
+    producer.send(Some(&event), Some(&key)).await?;
+
+    Ok(())
+}
+
+impl TryFrom<Action> for sea_orm_active_enums::Action {
+    type Error = Error;
+
+    fn try_from(v: Action) -> Result<Self> {
+        match v {
+            Action::Unspecified => Err(anyhow!("Invalid enum variant")),
+            Action::CreateDrop => Ok(Self::CreateDrop),
+            Action::MintEdition => Ok(Self::MintEdition),
+            Action::TransferAsset => Ok(Self::TransferAsset),
+            Action::RetryMint => Ok(Self::RetryMint),
+        }
+    }
+}
+
+impl TryFrom<Blockchain> for sea_orm_active_enums::Blockchain {
+    type Error = Error;
+
+    fn try_from(v: Blockchain) -> Result<Self> {
+        match v {
+            Blockchain::Unspecified => Err(anyhow!("Invalid enum variant")),
+            Blockchain::Solana => Ok(Self::Solana),
+            Blockchain::Polygon => Ok(Self::Polygon),
+            Blockchain::Ethereum => Ok(Self::Ethereum),
+        }
+    }
 }
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -4,7 +4,6 @@
 
 pub mod dataloaders;
 pub mod db;
-#[allow(clippy::pedantic)]
 pub mod entities;
 pub mod events;
 pub mod handlers;
@@ -32,15 +31,24 @@ use poem::{async_trait, FromRequest, Request, RequestBody};
 use queries::Query;
 
 use crate::dataloaders::DepositsLoader;
+impl hub_core::producer::Message for credits::CreditsEvent {
+    type Key = credits::CreditsEventKey;
+}
 
 #[allow(clippy::pedantic)]
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/organization.proto.rs"));
+    include!(concat!(env!("OUT_DIR"), "/credits_mpsc.rs"));
+}
+
+pub mod credits {
+    include!(concat!(env!("OUT_DIR"), "/credits.rs"));
 }
 
 #[derive(Debug)]
 pub enum Services {
     Organizations(proto::OrganizationEventKey, proto::OrganizationEvents),
+    CreditsMpsc(credits::CreditsEventKey, proto::CreditsMpscEvent),
 }
 
 impl hub_core::consumer::MessageGroup for Services {
@@ -58,6 +66,12 @@ impl hub_core::consumer::MessageGroup for Services {
                 let val = proto::OrganizationEvents::decode(val)?;
 
                 Ok(Services::Organizations(key, val))
+            },
+            "credits-mpsc" => {
+                let key = credits::CreditsEventKey::decode(key)?;
+                let val = proto::CreditsMpscEvent::decode(val)?;
+
+                Ok(Services::CreditsMpsc(key, val))
             },
             t => Err(RecvError::BadTopic(t.into())),
         }
